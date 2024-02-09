65d9051c48bc69eb36a6f9d6f72791c338c8e799
Daniel Bonasser <danielbonasser@gmail.com>
2024-02-08 21:00:48 -0300

    Transactions creation

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,25 @@
 import { FastifyInstance } from "fastify";
 import { knex } from "../database";
+import { randomUUID } from 'node:crypto'
+import { z } from "zod";
 
 export async function transactionRoutes(app: FastifyInstance) {
-    app.get('/hello', () => {
-        const tables = knex('sqlite_schema').select('*');
+    app.post('/', async (request, reply) => {
 
-        return tables;
+        const createTransactionBodySchema = z.object({
+            amount: z.number(),
+            title: z.string(),
+            type: z.enum(['credit', 'debit'])
+        })
+
+        const { amount, title, type } = createTransactionBodySchema.parse(request.body);
+
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1
+        });
+
+        return reply.status(201).send();
     });
 }
