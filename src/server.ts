16fe091ce999ecb2292074ead8d55dd51948fb0c
Daniel Bonasser <danielbonasser@gmail.com>
2024-02-16 19:56:16 -0300

    Using cookies in fastify

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
+import cookies from '@fastify/cookie';
 import { env } from "./env";
 import { transactionRoutes } from "./routes/transactions";
 
 const app = fastify();
 
+app.register(cookies);
 app.register(transactionRoutes, {
     prefix: 'transactions'
 });
